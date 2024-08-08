2f0f44ef4b0726a894a7b821c094498ac6067f85
Karla038 <karladk038@gmail.com>
2024-08-08 16:30:56 -0600

    Diseño de la pantalla principal

--- keep_going/lib/main.dart
+++ keep_going/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:keep_going/presentation/screens/home_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -24,6 +25,7 @@ class MyApp extends StatelessWidget {
                 ]
                 ),
             ),
+            child: const SafeArea(child: HomeScreen())
           ),
         ),
       ),
